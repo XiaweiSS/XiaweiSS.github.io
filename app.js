c7ccd41272c76c78f2b7f9451b577e454989d572
Xiawei <xiaweiss@foxmail.com>
2021-01-19 23:57:25 +0800

    Site updated: 2021-01-19 23:57:25

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = new Koa();
 
 // Force HTTPS using default resolver
 app.use(enforceHttps({
-  port: 443
+  port: 8081
 }));
 
 // index page
@@ -18,10 +18,10 @@ app.use(ctx => {
 
 // SSL options
 var options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.crt')
+  key: fs.readFileSync('/home/xiawei/xiawei.cc.key'),
+  cert: fs.readFileSync('/home/xiawei/xiawei.cc.pem')
 }
 
 // start the server
-http.createServer(app.callback()).listen(80);
-https.createServer(options, app.callback()).listen(443);
+http.createServer(app.callback()).listen(8080);
+https.createServer(options, app.callback()).listen(8081);
